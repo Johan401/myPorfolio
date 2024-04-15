97208d8be22b28bfbdaf846195c14a856e66b57b
Johan401 <johanzamora14@gmail.com>
2024-04-15 14:50:20 -0600

    Add AWS certifications and fix some responsive design issues

--- app/Certifications.jsx
+++ app/Certifications.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import "../css/certifications.css"
 import { PiCertificate } from "react-icons/pi";
 import AWSUdemy from "../public/certificates/AWSUdemy.jpg"
-import AWSComingSoon from "../public/certificates/AWSComingSoon.png"
+import AWSCertification from "../public/certificates/AWSCertification.png"
 import ReactUdemy from "../public/certificates/ReactUdemy.jpg"
 import SoftSkills from "../public/certificates/SoftSkills.png"
 
@@ -29,33 +29,18 @@ const Certifications = () => {
             pagination={{ clickable: true }}
             scrollbar={{ draggable: true }}
         >
-        <SwiperSlide>
-            <Image style={{ width: "100%", height: "100%", objectFit: "cover" }} src={AWSUdemy}></Image>
-        </SwiperSlide>
-        <SwiperSlide>
-            <Image style={{ width: "100%", height: "100%", objectFit: "cover" }} src={ReactUdemy}></Image>
-        </SwiperSlide>
-        <SwiperSlide>
-            <Image style={{ width: "100%", height: "100%", objectFit: "cover" }} src={SoftSkills}></Image>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div
-            style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'rgba(255, 255, 255, 0.5)',
-            zIndex: 1,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            }}>
-                <h3 style={{ fontWeight: "bold", zIndex: 2, color: '#000', textAlign: 'center' }}>Coming Soon</h3>
-            </div>
-            <Image style={{ filter: 'blur(3px)', width: "100%",  objectFit: "contain", }} src={AWSComingSoon}></Image>
-        </SwiperSlide>
+            <SwiperSlide>
+                <Image style={{ width: "100%", height: "100%", objectFit: "cover" }} src={AWSCertification}></Image>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Image style={{ width: "100%", height: "100%", objectFit: "cover" }} src={AWSUdemy}></Image>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Image style={{ width: "100%", height: "100%", objectFit: "cover" }} src={ReactUdemy}></Image>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Image style={{ width: "100%", height: "100%", objectFit: "cover" }} src={SoftSkills}></Image>
+            </SwiperSlide>
         </Swiper>
     </div>
   )
